a97ed659033d3d32774c421138da266628596ef3
ChocoliBao <837104975@qq.com>
2019-07-12 15:00:40 +0800

    init project

---
+++ src/main/java/cn/imustacm/decade/ImustacmDecadeApplication.java
@@ -0,0 +1,13 @@
+package cn.imustacm.decade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImustacmDecadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImustacmDecadeApplication.class, args);
+    }
+
+}
